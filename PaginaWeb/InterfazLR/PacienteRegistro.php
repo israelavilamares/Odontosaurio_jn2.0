035af2140e0e54109d9126c9183f1c58872e1fd1
israelavilamares <israeljoaquinavilamares@gmail.com>
2023-11-10 15:06:54 -0600

    changes

--- PaginaWeb/InterfazLR/PacienteRegistro.php
+++ PaginaWeb/InterfazLR/PacienteRegistro.php
@@ -15,14 +15,20 @@ if (isset($_POST['registrar'])) {
     $curp = $_POST["curp"];
     //funcion de comparacion que evita que registren a un usuario igual a otro
     $resultPac = verifPaciente($curp);
+    //llamamos a la variable de nombre contra la que va a comparar del respectivo nombre de campo
+    $nombreDoc = $_POST['nombre'];
+    //evita que registren a un doctor con el nombre de un paciente.
+    $resultDoc = verifDoctor($nombreDoc);
     //evita el registro de usuarios que ya estan el base de datos que no sea igual a el de administrador.
     if ($resultAdm->num_rows > 0) 
     {
         //mensaje error
-        echo '<div class="alerta">ya esta registrado ese Nombre. Intentelo de Nuevo con otro</div>';
+        echo '<div class="alerta">ya esta registrado ese Nombre. Intentelo de Nuevo con otro Nombre</div>';
     }elseif($resultPac->num_rows > 0)
     {        //mensaje error
         echo '<div class="alerta">ya esta registrado esa Curp. Intentelo de Nuevo</div>';
+    }elseif($resultDoc->num_rows > 0 ){
+        echo '<div class="alerta">ya esta registrado ese Nombre.Intentelo de Nuevo con otro Nombre</div>';
     }else{
 
                 //variables post
@@ -38,7 +44,7 @@ if (isset($_POST['registrar'])) {
                 //Esto es importante para prevenir ataques de inyección SQL, ya que asegura que los datos del usuario se manejen de manera segura.
                     if ($stmt->execute()) {                               
                         
-                        echo '<div class="alerta">ya esta registrado ese usuario.Intentelo de Nuevo</div>';
+                        echo '<div class="alerta">Registro exitoso</div>';
                         sleep(1);
                         // Registro exitoso                                                               
                         header("Location: FormPacientelogin.php");
